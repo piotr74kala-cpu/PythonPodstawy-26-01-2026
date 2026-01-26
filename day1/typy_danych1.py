0725da578e275ed991b3d79f6bdc35e7431f7129
radek <rajkonkret660@gmail.com>
2026-01-26 15:31:37 +0100

    [1 9] update typy_danych1.py

---
+++ day1/typy_danych1.py
@@ -0,0 +1,30 @@
+wiek = 47  # int
+rok = 2026  # int
+temp = 36.6  # float
+
+print(wiek + rok)  # 2073
+print(wiek - rok)  # -1979
+print(wiek * rok)  # 95222
+print(wiek / rok)  # 0.02319842053307009 -> float
+
+print(12 / 3)  # 4.0
+print(2026 / 47)  # 43.1063829787234
+print(2026 // 47)  # część cąlkowita z dzielenia, 43
+
+print(rok % wiek)  # % - modulo, reszta z dzielenia, 5
+print(43 * 47)
+print(2026 - 2021)  # 5
+
+print(10 % 3)  # reszta 1
+
+print(wiek ** rok)  # potęgowanie
+
+print(len(str(wiek ** rok)))  # 3388 znaków
+# print(len(str(wiek ** rok ** 2)))
+# ValueError: Exceeds the limit (4300 digits) for integer string conversion;
+# use sys.set_int_max_str_digits() to increase the limit
+
+print(54 - 5 * 43 + 4 / 2 + 4 / 2)  # -157.0
+print(54 - 5 * 43 + (4 / 2 + 4) / 2)  # -158.0
+
+
