65a28d42c91eedc1076cc20cc2e19efc9ee72eb7
radek <rajkonkret660@gmail.com>
2026-01-26 13:12:54 +0100

    [1 4] update teskty.py

---
+++ day1/teskty.py
@@ -0,0 +1,13 @@
+tekst = "Witaj Świecie"
+print(tekst)
+print(type(tekst))
+# C:\Users\CSComarch\PycharmProjects\PythonPodstawy-26-01-2026\.venv\Scripts\python.exe C:\Users\CSComarch\PycharmProjects\PythonPodstawy-26-01-2026\day1\teskty.py
+# Witaj Świecie
+# <class 'str'>
+
+""" Return a copy of the string converted to uppercase. """
+tekst.upper()
+print(tekst)
+print(tekst.upper()) # WITAJ ŚWIECIE
+tekst_upper = tekst.upper()
+print(tekst_upper) # WITAJ ŚWIECIE
