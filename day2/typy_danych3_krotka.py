09ab392d640117a33cd92664d9f53db8e18d23ab
radek <rajkonkret660@gmail.com>
2026-01-27 11:57:55 +0100

    [2 9] update typy_danych3_krotka.py

--- day2/typy_danych3_krotka.py
+++ day2/typy_danych3_krotka.py
@@ -28,8 +28,50 @@ print(type(tupla_jeden))
 del tupla_jeden
 # print(tupla_jeden)  # NameError: name 'tupla_jeden' is not defined
 
+# ('Zenek', 'MArek', 'Radek', 'Ania')
 print(tupla_imiona.index("Radek"))  # indeks numer: 2
 print(tupla_imiona.count("Radek"))  # występuje 1 raz
 
 print(len(tupla_imiona))  # długośc 4
 
+tup = 1, 2
+print(type(tup))  # <class 'tuple'>
+
+# a - pierwsza wartosc, b - druga wartość
+a = tup[0]
+b = tup[1]
+print(a, b)  # 1 2
+
+# rozpakowanie krotki
+a, b = 1, 2
+print(a, b)  # 1 2
+
+a, b = tup
+print(a, b)  # 1 2
+
+print(tupla_imiona)  # ('Zenek', 'MArek', 'Radek', 'Ania')
+print(len(tupla_imiona))  # 4
+# name1, name2, name3
+# ValueError: too many values to unpack (expected 3, got 4)
+# name1, name2, name3 = tupla_imiona
+name1, name2, *name3 = tupla_imiona  # * worek na pozostałę dane
+print(name1, name2, name3)  # Zenek MArek ['Radek', 'Ania']
+print(name1, name2, *name3)  # Zenek MArek Radek Ania - rozpakowało kolekcję
+
+name1, *name2, name3 = tupla_imiona  # * worek na pozostałę dane
+print(name1, name2, name3)  # Zenek ['MArek', 'Radek'] Ania
+
+*name1, name2, name3 = tupla_imiona  # * worek na pozostałę dane
+print(name1, name2, name3)  # Zenek ['MArek', 'Radek'] Ania
+
+# sortowanie sorted() - zwraca posortowaną listę!!!
+print(sorted(tupla_imiona))
+# ['Ania', 'MArek', 'Radek', 'Zenek']
+
+sortowana = sorted(tupla_imiona)
+print(id(sortowana))
+print(id(tupla_imiona))
+# 2318260656384
+# 2318266518640
+# tupla imiona nie została zmieniona (jest niemutowalna)
+print(tupla_imiona)  # ('Zenek', 'MArek', 'Radek', 'Ania')
