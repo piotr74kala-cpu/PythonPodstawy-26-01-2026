afeb60878e675ac9d1566c4ff6873f27f96addb6
radek <rajkonkret660@gmail.com>
2026-01-27 15:42:20 +0100

    [2 20] wyjatki.py

---
+++ day2/warunki.py
@@ -0,0 +1,39 @@
+# warunki
+# instrukcje warunkowe
+# instrukcje sterowanie przepływem programu
+# if
+# w zależności od warunku wykona jeden lub drugi blok programu
+# wyrażenie w warunku musi zwrócic typ bool
+
+odp = True
+if odp:
+    print("Test")
+
+if odp: print("Test")
+
+# odp = False
+if odp:  # blok programu wykonany gdy warunek True
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+
+print("Dalsza część programu")
+
+odp = "Radek"
+if odp:
+    print("Dane zostały wczytane")
+# Dane zostały wczytane
+
+if odp == "Radek":  # porównanie
+    print("Jesteś Radek")
+# Jesteś Radek
+
+odp = 0
+if odp:
+    print("Działa")
+else: # w innym przypadku, wartość domyślna
+    print("Zero -> False")
+# Zero -> False
