2ca4b99ca874dc49d56c4a85f06396f108a50298
radek <rajkonkret660@gmail.com>
2026-01-27 13:00:30 +0100

    [2 14] update typy_danych4_zbior.py

---
+++ day2/typy_danych5_slownik.py
@@ -0,0 +1,31 @@
+# słownik - para klucz:wartosc
+# {"user" : "Radek"}
+# odpowiednik jsona
+# klucze nie mogą się powtarzac
+
+# pusty słownik
+dictionary = {}
+print(dictionary)  # {}
+print(type(dictionary))  # <class 'dict'>
+
+dictionary_1 = dict()
+print(dictionary_1)  # {}
+print(type(dictionary_1))  # <class 'dict'>
+
+# dodanie eleemntów do słownika
+dictionary['imie'] = "Radek"
+print(dictionary)  # {'imie': 'Radek'}
+# dodac klucz wiek
+dictionary['wiek'] = 50
+print(dictionary)  # {'imie': 'Radek', 'wiek': 50}
+
+print(dictionary.keys())  # dict_keys(['imie', 'wiek'])
+print(dictionary.values())  # dict_values(['Radek', 50])
+print(dictionary.items())  # dict_items([('imie', 'Radek'), ('wiek', 50)])
+
+# nadpisanie elementu
+dictionary['imie'] = "Tomek"
+print(dictionary)  # {'imie': 'Tomek', 'wiek': 50}
+
+# wypisanie wartości dla klucza
+print(dictionary['imie'])  # Tomek
