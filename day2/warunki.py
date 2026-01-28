c15c531435f5700d84ba9ee3f7325cf0522a6da9
radek <rajkonkret660@gmail.com>
2026-01-28 10:08:09 +0100

    [3 2] update warunki.py

--- day2/warunki.py
+++ day2/warunki.py
@@ -52,3 +52,69 @@ if n > 3:
 # walrus operator, operator morsa
 if (n := len(a)) > 3:
     print(f"Długość wynosi: {n}, wicej niż 3")
+#
+# podatek = 0
+#
+# zarobki = int(input("Podaj zarobki:"))
+#
+# if zarobki < 10_000:
+#     podatek = 0
+# elif zarobki < 40_000:
+#     podatek = 0.2
+# elif zarobki < 100_000:
+#     podatek = 0.4
+# else:
+#     podatek = 0.9
+#
+# print(f"Podatek wynosi: {zarobki * podatek} pln.")
+# dodac podatek 20%, dla zarobków od 10000 do 39999
+# Podaj zarobki:15000
+# Podatek wynosi: 3000.0 pln.
+
+sum_zam = 150
+if sum_zam > 100:
+    rabat = 25
+else:
+    rabat = 0
+
+print(f"Rabat wynosi: {rabat}")
+# Rabat wynosi: 25
+
+# operator warunkowy
+rabat = 25 if sum_zam > 100 else 0
+print(f"Rabat wynosi: {rabat}")
+
+# napisac tezt z...
+# trzy pytania
+# dodac punktację
+
+# punkty = 0
+# odp = input("podaj rok Chrztu Polski")
+# if odp.strip().casefold() == '966':
+#     print("Prawidłowa odpowiedź")
+#     # punkty = punkty + 1
+#     punkty += 1
+# else:
+#     print("Doucz się")
+#
+# odp = input("Czy Ala ma kota?")
+# if odp.strip().casefold() == 'tak'.strip().casefold():
+#     print("Prawidłowa odpowiedź")
+#     # punkty = punkty + 1
+#     punkty += 1
+# else:
+#     print("Doucz się")
+#
+# print("Zdobyłes pkt:", punkty)
+# print(f"Zdobyłes pkt: {punkty}")
+# podaj rok Chrztu Polski966
+# Prawidłowa odpowiedź
+# Czy Ala ma kota?tak
+# Prawidłowa odpowiedź
+# Zdobyłes pkt: 2
+# Zdobyłes pkt: 2
+
+# zasymulejemy system zbierania logów
+# zmienna: przechowuje typ systemu, console, email, inny
+# console: "Stało się coś strasznego"
+
