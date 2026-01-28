40015e9c550ff00eb32108437ea215aa611d4064
radek <rajkonkret660@gmail.com>
2026-01-28 10:50:46 +0100

    [3 5] update random_zad1.py

---
+++ day3/random_zad1.py
@@ -0,0 +1,26 @@
+import random
+
+# działania na liczbach losowych
+
+#  """Return random integer in range [a, b], including both end points.
+#        """
+print(random.randint(1, 100))  # int od 1 do 100 włacznie
+
+print(random.randrange(1, 100))  # od 1 do 99
+print(random.randrange(5))  # int od 0 do 4
+
+print(random.random())  # float, 0.08086122087233028, od 0 do 0.999999999
+print(random.random() * 7)  # float, 4.062106231875812, od 0 do 6.999999999
+print(round(random.random() * 6))  # 4
+
+lista = [67, 45, 32, 68, 90, 42, 69]
+print(lista[random.randrange(len(lista))])  # element: 42
+
+print(random.choice(lista))  # 67
+
+# stworzyc listę kul
+# wylosować kulę z listy
+# wyświetlic kulę
+# usunąc kulę
+# ctrl shift f - wyszukiwanie w całym projekcie
+
