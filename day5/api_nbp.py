7490314bbbab2c697265a950672ad217ea46e247
radek <rajkonkret660@gmail.com>
2026-01-30 12:07:26 +0100

    [5 15] update api_nbp.py

--- day5/api_nbp.py
+++ day5/api_nbp.py
@@ -12,3 +12,5 @@ print(data)
 print(data['rates'])  # [{'no': '020/A/NBP/2026', 'effectiveDate': '2026-01-30', 'mid': 4.2131}]
 print(data['rates'][0])  # {'no': '020/A/NBP/2026', 'effectiveDate': '2026-01-30', 'mid': 4.2131}
 print(data['rates'][0]['mid'])  # 4.2131
+
+# https://github.com/public-api-lists/public-api-lists
