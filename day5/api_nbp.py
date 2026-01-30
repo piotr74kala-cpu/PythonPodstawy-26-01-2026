4e9ae89596e176e4bf800cd5eb8ad53047b96f32
radek <rajkonkret660@gmail.com>
2026-01-30 12:01:46 +0100

    [5 13] update api_nbp.py

--- day5/api_nbp.py
+++ day5/api_nbp.py
@@ -1,1 +1,14 @@
+import requests
+
 url = "https://api.nbp.pl/api/exchangerates/rates/A/eur/"
+
+response = requests.get(url)
+
+data = response.json()
+print(data)
+# {'table': 'A', 'currency': 'euro', 'code': 'EUR',
+# 'rates': [{'no': '020/A/NBP/2026', 'effectiveDate': '2026-01-30', 'mid': 4.2131}]}
+
+print(data['rates'])  # [{'no': '020/A/NBP/2026', 'effectiveDate': '2026-01-30', 'mid': 4.2131}]
+print(data['rates'][0])  # {'no': '020/A/NBP/2026', 'effectiveDate': '2026-01-30', 'mid': 4.2131}
+print(data['rates'][0]['mid'])  # 4.2131
