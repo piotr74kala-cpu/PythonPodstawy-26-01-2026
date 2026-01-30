6be480b7a3b903d09581ebaf3635f9da46c735a3
radek <rajkonkret660@gmail.com>
2026-01-30 10:04:00 +0100

    [5 1] update kl4

--- day5/kl4.py
+++ day5/kl4.py
@@ -20,7 +20,7 @@ class Ptak(ABC):
     def latam(self):
         print("Tu", self.gatunek, "Lecę z szybkością", self.szybkosc, "km/h")
 
-    @abstractmethod # dekorator, metoda abstrakcyjna
+    @abstractmethod  # dekorator, metoda abstrakcyjna
     def wydaj_odglos(self):
         pass
 
@@ -40,6 +40,23 @@ class Kura(Ptak):
     def wydaj_odglos(self):
         print("ko ko ko koko")
 
+
+class Orzel(Ptak):
+    """
+    Klasa dziedziczy po klasie Orzeł
+    """
+
+    # musimy nadpisac metode abstrakcyjną
+    def wydaj_odglos(self):
+        print("Kir kier kir kier")
+
+
+class Sowa(Ptak):
+    """
+    Klasa Sowa
+    """
+
+
 # klasa abstrakcyjna
 # nie można tworzyć obiektów tej klasy
 # TypeError: Can't instantiate abstract class Ptak without an implementation for abstract method 'wydaj_odglos'
@@ -52,3 +69,29 @@ class Kura(Ptak):
 kur2 = Kura("Kura zielononóżka")
 kur2.latam()  # Tu Kura zielononóżka Ja nie latam
 kur2.wydaj_odglos()  # ko ko ko koko
+
+or2 = Orzel("Orzel Bielik", 55)
+or2.latam()
+or2.wydaj_odglos()
+# Tu Orzel Bielik Lecę z szybkością 55 km/h
+# Kir kier kir kier
+
+# TypeError: Can't instantiate abstract class Sowa without an implementation for abstract method 'wydaj_odglos'
+# nie ma metody wydaj_odglos
+# nie można stworzyc obiektu tej klasy
+# sowa = Sowa("Sowa", 15)
+
+lista = [or2, kur2]  # obiekty różnych klas
+# polimorfizm
+# obiekty różnych klas maja poprzez wspólna kalsę abstrakcyjną wspolne cechy, metody
+
+for i in lista:
+    i.wydaj_odglos()
+    print(i.__class__.__name__)
+# Kir kier kir kier
+# ko ko ko koko
+# Kir kier kir kier
+# Orzel
+# ko ko ko koko
+# Kura
+# ctrl alt l - formatowanie kodu
